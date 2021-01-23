f00434a827f44ec758e34505c964ca3dfb780661
Vipin Viswanath <vipinvis91@gmail.com>
2021-01-23 17:16:20 +0530

    Update Shipping cost page

--- src/app/pages/components/shipping-charge/shipping-charge.component.ts
+++ src/app/pages/components/shipping-charge/shipping-charge.component.ts
@@ -11,34 +11,63 @@ export class ShippingChargeComponent implements OnInit {
 
   expressCharge: number = 0;
   normalCharge: number = 0;
+  shippingTypes: any = [];
+  shippingTypeCharge: any = [];
 
   constructor(private _http: HttpService,
-    private _sharedService: SharedService) { }
+    private _sharedService: SharedService) {
+  }
 
   ngOnInit(): void {
+    this.shippingTypes = [];
+    this.shippingTypeCharge = [];
+    this._http.getShippingTypes().subscribe((res) => {
+      this.shippingTypes = JSON.parse(JSON.stringify(res));
+      this.shippingTypes.forEach((type: any) => {
+        this.shippingTypeCharge.push({
+          "name": type,
+          "cost": ""
+        })
+      });
+    });
+
+    //To remove:
+    this.shippingTypes = ["as","sdsd", "wsadsfd","sfsf", "sfsf", "sfsf"];
+    this.shippingTypes.forEach((type: any) => {
+      this.shippingTypeCharge.push({
+        "name": type,
+        "cost": ""
+      })
+    });
+
   }
   /**
    * 
    */
   saveCharge() {
-    let shippingCharge = {
-      "user_name": this._sharedService.loggedUser,
-      "express": this.expressCharge,
-      "normal": this.normalCharge
+    let shippingCharge: any = {
+      "user_name": this._sharedService.username,
+      "shipping": []
     }
 
+    this.shippingTypeCharge.forEach((type: any) => {
+      shippingCharge['shipping'].push({
+        "name": type.name,
+        "cost": type.cost
+      })
+    });
     this._http.updateShipppingCharge(shippingCharge).subscribe((res: any) => {
       this._sharedService.openSnackBar("Shipping Charge Updated Successfully!!");
     }, (err: any) => {
       console.error(err);
     })
   }
-/**
- * 
- */
-cancel() {
-  this.expressCharge = 0;
-  this.normalCharge = 0;
-}
+  /**
+   * 
+   */
+  cancel() {
+    this.expressCharge = 0;
+    this.normalCharge = 0;
+  }
 
 }
